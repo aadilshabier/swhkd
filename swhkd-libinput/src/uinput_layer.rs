1d026d6685f09bd81b769af86170bdbd959338a5
Mohammad Aadil Shabier <aadilshabier1@gmail.com>
2025-07-09 20:44:35 +0530

    Downgrade version numbers to match swhkd

--- swhkd-libinput/src/uinput_layer.rs
+++ swhkd-libinput/src/uinput_layer.rs
@@ -3,11 +3,14 @@ use uinput;
 
 pub use uinput::Device;
 
+pub const EV_KEY: i32 = 0x01;
+
 pub fn build_uinput_dev() -> Result<uinput::Device, uinput::Error> {
     use uinput::event;
     let mut builder = uinput::default()?;
     // Keyboard
     builder = builder.name("uinput device")?.event(event::Keyboard::All)?;
+    builder = builder.name("uinput device")?.event(event::Event::All)?;
     // Mouse buttons
     for event in event::controller::Mouse::iter_variants() {
         builder = builder.event(event)?;
@@ -32,7 +35,7 @@ pub fn emit_libinput_keyboard_event(
     let state = keyboard_event.key_state() as i32;
     log::info!("Device: {device_name}, key: {key_code}, state: {state:?}");
 
-    device.write(1, key_code, 1 - state)?;
+    device.write(EV_KEY, key_code, 1 - state)?;
     device.synchronize()?;
     Ok(())
 }
@@ -45,6 +48,7 @@ pub fn emit_libinput_pointer_event(
     match pointer_event {
         Motion(motion_event) => {
             use uinput::event::{Relative, relative::Position};
+            // TODO: check if mouse acceleration is enabled
             let dx = motion_event.dx_unaccelerated();
             let dy = motion_event.dy_unaccelerated();
             log::info!("Mouse: {}, dx: {dx:.2}, dy: {dy:.2}", motion_event.device().name(),);
@@ -59,13 +63,14 @@ pub fn emit_libinput_pointer_event(
                 "Mouse: {}, button: {button}, state: {state:?}",
                 button_event.device().name(),
             );
-            device.write(1, button, 1 - state)?;
+            device.write(EV_KEY, button, 1 - state)?;
             device.synchronize()?;
         }
         ScrollWheel(scrollwheel_event) => {
+            use input::event::pointer::Axis;
             use uinput::event::{Relative, relative::Wheel};
-            let vert = scrollwheel_event.scroll_value_v120(input::event::pointer::Axis::Vertical);
-            let hori = scrollwheel_event.scroll_value_v120(input::event::pointer::Axis::Horizontal);
+            let vert = scrollwheel_event.scroll_value_v120(Axis::Vertical);
+            let hori = scrollwheel_event.scroll_value_v120(Axis::Horizontal);
             log::info!(
                 "Mouse: {}, wheel vert: {vert}, hori: {hori}",
                 scrollwheel_event.device().name(),
